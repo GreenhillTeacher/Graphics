95a969e4816f9be3bcaede4af54a109f6908cb9e
suarezm <suarezm@greenhill.org>
2019-10-10 10:00:32 -0500

    Initial commit

---
+++ src/DrawingShapes.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.canvas.*;
+import javafx.stage.Stage;
+
+public class DrawingShapes extends Application{
+
+        @Override
+        public void start(Stage myStage) throws Exception{
+            GraphicsContext gc = new GraphicsContext();
+        }
+
+
+        public static void main(String[] args) {
+            launch(args);
+        }
+    }
+
+}
